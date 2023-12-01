a6493faad61cc60f306b1c48e817cc75bd201a8c
RadhikaEk21 <radhikaoneteam21@gmail.com>
2023-12-01 11:08:01 +0530

    second commit

--- crud_project/crd_project/crd_app/urls.py
+++ crud_project/crd_project/crd_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('reg/',views.register)
+    path('reg/',views.register),
+    path('msg/',views.msg)
     
 ]
--- crud_project/crd_project/crd_app/views.py
+++ crud_project/crd_project/crd_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
     return render(request,'reg.html')
+
+def msg(request):
+    return HttpResponse("hi")
